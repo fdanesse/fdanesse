ee1e2b8085c6958fe737ecb1791becf9bba14f46
Flavio Danesse <fdanesse@gmail.com>
2021-04-26 18:41:01 -0300

    correccion de detalles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseconf: {
+    apiKey: 'AIzaSyCRzt-miJGUI3icim5NxH1TeVwH9jLULYg',
+    authDomain: 'fdanesse-f2b2c.firebaseapp.com',
+    databaseURL: 'https://fdanesse-f2b2c.firebaseio.com',
+    projectId: 'fdanesse-f2b2c',
+    storageBucket: 'fdanesse-f2b2c.appspot.com',
+    messagingSenderId: '943272563535'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseconf: {
+    apiKey: 'AIzaSyCRzt-miJGUI3icim5NxH1TeVwH9jLULYg',
+    authDomain: 'fdanesse-f2b2c.firebaseapp.com',
+    databaseURL: 'https://fdanesse-f2b2c.firebaseio.com',
+    projectId: 'fdanesse-f2b2c',
+    storageBucket: 'fdanesse-f2b2c.appspot.com',
+    messagingSenderId: '943272563535'
+  }
 };
